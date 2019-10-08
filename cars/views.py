f9f0a7be4880b3b000c282b9d9282d5aa8854950
Fay Alawadhi <fayalawadhi@Fays-MacBook-Pro.local>
2019-10-08 18:04:19 +0300

    DONE

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages 
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +20,34 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
-
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+		"form":form,
+	}
+	return render(request, 'create.html', context)
+	
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance=car_obj)
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES, instance=car_obj)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'This file has been updated!')
+			return redirect('car-list')
+	context = {
+		"car_obj": car_obj, 
+		"form":form,
+	}
+	return render(request, 'update.html', context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	Car.objects.get(id=car_id).delete()
+	return redirect('car-list')
